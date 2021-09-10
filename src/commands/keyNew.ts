a9348e2bb22481a6cd29b06fd571d5438d0dcea8
Steve Korshakov <steve@korshakov.com>
2021-09-10 22:10:37 +0300

    feat: first command

---
+++ src/commands/keyNew.ts
@@ -0,0 +1,22 @@
+import prompt from 'prompt';
+import { TonClient } from 'ton';
+import fs from 'fs';
+
+export async function keyNew() {
+    prompt.start({ 'message': 'ton-cli' });
+    const { name, masterchain } = await prompt.get([{
+        name: 'name'
+    }, {
+        name: 'masterchain',
+        type: 'boolean',
+        default: false
+    }]);
+    const client = new TonClient({ endpoint: 'https://ton.korshakov.com/jsonRpc' });
+    let wallet = await client.createNewWallet({ workchain: masterchain ? -1 : 0 });
+    console.log('Address: ' + wallet.wallet.address.toFriendly());
+    console.log('Mnemonics: ' + wallet.mnemonic.join(' '));
+    fs.writeFileSync(name + '.addr', wallet.wallet.address.toFriendly(), 'utf-8');
+    fs.writeFileSync(name + '.backup', wallet.mnemonic.join(' '), 'utf-8');
+    fs.writeFileSync(name + '.public', wallet.key.publicKey);
+    fs.writeFileSync(name + '.secret', wallet.key.secretKey);
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('Hello World!');
+import yargs from 'yargs';
+import { keyNew } from './commands/keyNew';
+
+yargs.scriptName('ton-cli')
+    .usage('$0 <cmd> [args]')
+    // .command('export', 'Export key from mnemonics', () => { }, async () => {
+
+    // })
+    .command('new-key', 'Create new key', () => { }, async () => {
+        await keyNew();
+    })
+    .help()
+    .argv;
