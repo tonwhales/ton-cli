e6cb0164bff605d8f3b370e3bd76a7ad641a951e
Dan Volkov <hi@dvlkv.com>
2023-02-23 02:20:46 +0400

    chore: update license

--- src/commands/viewKeystore.ts
+++ src/commands/viewKeystore.ts
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) Whales Corp. 
+ * All Rights Reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 import { Cell, fromNano, KeyStore, SendMode, toNano, validateWalletType } from "ton";
 import { askPassword } from "./utils/askPassword";
 import { openKeystore } from "./utils/openKeystore";
--- src/commands/writeRawWallet.ts
+++ src/commands/writeRawWallet.ts
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) Whales Corp. 
+ * All Rights Reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 import { mnemonicToWalletKey } from "ton-crypto";
 import { Config } from "../Config";
 import { askAddress } from "./utils/askAddress";
